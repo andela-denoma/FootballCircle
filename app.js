7e006b50bdfb4b285ad28894bcdcb77076e2caef
Enoma Deborah <deborah.enomah@andela.co>
2015-01-12 17:49:41 +0100

    set env

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var forum = require('./routes/forum');
 var topic = require('./routes/topic');
 var comment = require('./routes/comment');
 
+var port = process.env.PORT || 3000;
 
 app.use('/forums', forum);
 app.use('/forums/:forums_id/topics', topic);
 app.use('/forums/:forums_id/topics/:topics_id/comments', comment);
 
- app.listen(3000); //function(){
+ app.listen(port); //function(){
   // console.log('server started');
 // });
