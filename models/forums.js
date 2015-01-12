34d50a324d08b8f0c459b50e0f72bf7163dd78d9
Enoma Deborah <deborah.enomah@andela.co>
2015-01-12 18:14:38 +0100

    modified

--- models/forums.js
+++ models/forums.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://football:osayande@ds031631.mongolab.com:31631/football');
 
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function (callback) {
-//   console.log('...connected')
-// });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('...connected')
+});
 
 var Schema = mongoose.Schema;
 
