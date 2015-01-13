222b7bffeb41648a77ee16f43944dfe49963ea5a
Enoma Deborah <deborah.enomah@andela.co>
2015-01-13 12:16:08 +0100

    allow cross origin

--- app.js
+++ app.js
@@ -8,16 +8,17 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
   };
 
+app.set('port', (process.env.PORT || 3000));
 app.use(allowCrossDomain);
-
-var port = process.env.PORT || 3000;
-
 app.use('/forums', forum);
 app.use('/forums/:forums_id/topics', topic);
 app.use('/forums/:forums_id/topics/:topics_id/comments', comment);
 
-app.listen(port, function(){
-  console.log('server started' + port);
+
+
+app.listen(app.get('port'), function(){
+  console.log('server started');
 });
