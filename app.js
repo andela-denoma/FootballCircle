d52abf5ece788e740e75bb3588d134823360164b
Enoma Deborah <deborah.enomah@andela.co>
2015-01-13 11:16:05 +0100

    modified

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ app.use('/forums/:forums_id/topics/:topics_id/comments', comment);
 
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   };
 
   app.use(allowCrossDomain);
-  
+
  app.listen(port, function(){
   console.log('server started');
 });
