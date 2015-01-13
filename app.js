37a1939571e157aa2fdc761bc4c059f52dddaa64
Enoma Deborah <deborah.enomah@andela.co>
2015-01-13 11:26:23 +0100

    allow cross origin

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var app = express();
 var forum = require('./routes/forum');
 var topic = require('./routes/topic');
 var comment = require('./routes/comment');
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  };
+
+app.use(allowCrossDomain);
 
 var port = process.env.PORT || 3000;
 
@@ -10,14 +17,6 @@ app.use('/forums', forum);
 app.use('/forums/:forums_id/topics', topic);
 app.use('/forums/:forums_id/topics/:topics_id/comments', comment);
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  };
-
-  app.use(allowCrossDomain);
-
- app.listen(port, function(){
+app.listen(port, function(){
   console.log('server started');
 });
