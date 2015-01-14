a135306c5332dcb639c66c50a942d3682fe88b60
Enoma Deborah <deborah.enomah@andela.co>
2015-01-14 12:37:02 +0100

    allowed delete

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var comment = require('./routes/comment');
 
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next();
   };
